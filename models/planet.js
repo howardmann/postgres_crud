041ae704c25b169eaa9245aabdd5df1d6670d9bc
howardmann <howardmann27@gmail.com>
2017-12-25 10:15:06 +1100

    Update queries

--- models/planet.js
+++ models/planet.js
@@ -9,12 +9,9 @@ Planet.find = () => {
   return knex.raw(`
     SELECT planets.*, json_agg(races.name) AS races, json_agg(champions.name) AS champions
       FROM planets
-    LEFT OUTER JOIN planets_races
-      ON planets_races.planet_id = planets.id
-    LEFT OUTER JOIN races
-      ON planets_races.race_id = races.id
-    LEFT OUTER JOIN champions
-      ON champions.race_id = races.id
+    LEFT OUTER JOIN planets_races ON planets_races.planet_id = planets.id
+    LEFT OUTER JOIN races ON planets_races.race_id = races.id
+    LEFT OUTER JOIN champions ON champions.race_id = races.id
     GROUP BY planets.id
     ORDER BY planets.id
   `)
